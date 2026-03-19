050401876c20c3dc012cfc3542185b7bb04182b7
vectorescomprados-pixel <vectorescomprados@gmail.com>
2026-03-19 13:48:12 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'uf-cache-v1';
+const ASSETS = ['./', 'index.html', 'style.css', 'app.js', 'manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
